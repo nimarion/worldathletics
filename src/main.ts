518bfa197d24a243cd8745cf701450f982a9c803
Niklas Marion <niklas.marion@gmx.de>
2023-11-21 12:41:32 +0100

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   Sentry.init();
   const config = new DocumentBuilder()
     .setTitle('WorldAthletics Proxy API')
