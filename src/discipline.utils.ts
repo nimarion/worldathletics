e307f753fca66859c952c760eebd9ecbc48f5aca
Niklas Marion <niklas.marion@gmx.de>
2023-12-23 09:03:50 +0100

    add missing code, remove sh suffix for short track disciplines

--- src/discipline.utils.ts
+++ src/discipline.utils.ts
@@ -1,8 +1,15 @@
 import * as disciplines from './disciplines.json';
 
 export default function mapDisciplineToCode(discipline: string): string {
+  if (discipline == '3x800 Metres Relay') {
+    return '3X8';
+  }
   const disciplineItem = disciplines.find((d) => d.discipline === discipline);
   if (disciplineItem) {
+    const disciplineCode = disciplineItem.disciplineCode;
+    if (disciplineCode.endsWith('sh')) {
+      return disciplineCode.substring(0, disciplineCode.length - 2);
+    }
     return disciplineItem.disciplineCode;
   }
   throw new Error(`Discipline ${discipline} not found`);
