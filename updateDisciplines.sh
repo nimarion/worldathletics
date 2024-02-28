091257985397c07fcb15f36e73fa605d00f83374
Niklas Marion <niklas.marion@gmx.de>
2024-02-28 22:31:27 +0100

    pretty print json for better diff

--- updateDisciplines.sh
+++ updateDisciplines.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-curl -o src/disciplines.json --silent --show-error --fail https://worldathletics.pfingstsportfest.de/disciplines
+curl -o src/disciplines.json --silent --show-error --fail https://worldathletics.pfingstsportfest.de/disciplines 
+jq . src/disciplines.json > src/disciplines_pretty.json
+rm src/disciplines.json
+mv src/disciplines_pretty.json src/disciplines.json
